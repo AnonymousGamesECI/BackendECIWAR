1381c756ab79b17ee4deaf884f67a1aa22ff456c
rescate rescate <rescate@labinfo.is.escuelaing.edu.co>
2018-03-24 11:40:31 -0500

    Frontend added

--- src/main/java/edu/eci/arsw/collabpaint/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/WebSocketConfig.java
@@ -34,7 +34,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/room");
         config.setApplicationDestinationPrefixes("/app");        
     }
 
