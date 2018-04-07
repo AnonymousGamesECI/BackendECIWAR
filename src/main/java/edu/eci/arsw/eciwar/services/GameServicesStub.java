e9e89d29ee0a170189a5c204b6ce45fc77d96e9f
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-07 09:45:25 -0500

    Services updated

--- src/main/java/edu/eci/arsw/eciwar/services/GameServicesStub.java
+++ src/main/java/edu/eci/arsw/eciwar/services/GameServicesStub.java
@@ -7,6 +7,8 @@ package edu.eci.arsw.eciwar.services;
 
 import edu.eci.arsw.eciwar.model.Player;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,20 +17,46 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GameServicesStub implements GameServices{
+    ConcurrentHashMap<Integer, Set<Player>> roomsData = new ConcurrentHashMap<>();
+    
+    public GameServicesStub(){
+        roomsData.put(1, new ConcurrentSkipListSet<>());
+    }
 
     @Override
     public void registerPlayerToRoom(int roomId, Player pl) throws ServicesException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!roomsData.containsKey(roomId)){
+            throw new ServicesException("Room "+roomId+" not registered in the server.");
+        }
+        else{
+            if (roomsData.get(roomId).contains(pl)){
+                throw new ServicesException("Player "+pl.getId()+" already registered in room "+roomId);
+            }
+            else{
+                roomsData.get(roomId).add(pl);
+            }
+            
+        }
     }
 
     @Override
     public void removePlayerFromRoom(int roomId, Player pl) throws ServicesException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!roomsData.containsKey(roomId)){
+            throw new ServicesException("Room "+roomId+" not registered in the server.");
+        }
+        else{
+            if (!roomsData.get(roomId).contains(pl)){
+                throw new ServicesException("Player "+pl.getId()+" not registered in room "+roomId);
+            }
+            else{
+                roomsData.get(roomId).remove(pl);
+            }            
+        }
     }
 
     @Override
     public Set<Player> getRegisteredPlayers(int roomId) throws ServicesException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return roomsData.get(roomId);
     }
     
 }
