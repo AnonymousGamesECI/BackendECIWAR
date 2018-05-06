aef5b0f12c1f538da34eadc6ed25a228049d2356
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-05-05 23:01:28 -0500

    REDIS template fixed

--- src/main/java/edu/eci/arsw/eciwar/services/REDISGameServices.java
+++ src/main/java/edu/eci/arsw/eciwar/services/REDISGameServices.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -26,48 +27,32 @@ public class REDISGameServices implements GameServices{
     
     @Override
     public void registerPlayerToRoom(int roomId, Player pl) throws ServicesException {
-        String actGame = "room:" + roomId;
-        if (!template.hasKey(actGame)) {
-            throw new ServicesException("La sala no se encuentra creada.");
-        } else {
-            template.opsForList().leftPush(actGame + ":players", String.valueOf(pl.getId()));
-        }
+        template.opsForSet().add(String.valueOf(roomId), String.valueOf(pl.getId()));
     }
 
     @Override
     public void removePlayerFromRoom(int roomId, Player pl) throws ServicesException {
-        String actGame = "room:" + roomId;
-        if (!template.hasKey(actGame)) {
-            throw new ServicesException("La sala no se encuentra creada.");
-        } else {
-            template.opsForList().remove(actGame + ":players", 1, pl.getId());
-        }
+        template.opsForSet().remove(String.valueOf(roomId), String.valueOf(pl.getId()));
     }
 
     @Override
     public Set<Player> getRegisteredPlayers(int roomId) throws ServicesException {
-        String actGame = "room:" + roomId;
-        List<String> result = template.opsForList().range(actGame, 0, -1);
-        Set<Player> players = new HashSet();
-        result.forEach((idPlayer) -> {
-            players.add(new Player(Integer.valueOf(idPlayer)));
-        });
-        return players;
+        Set<String> players = template.opsForSet().members(String.valueOf(roomId));
+        Set<Player> setOfPlayers = new ConcurrentSkipListSet();
+        for(String i: players){
+            setOfPlayers.add(new Player(Integer.valueOf(i)));
+        }
+        return setOfPlayers;
     }
 
     @Override
     public void createRoom(int roomId) throws ServicesException {
-        String actGame = "room:" + roomId;
-        if (template.hasKey(actGame)) {
-            throw new ServicesException("La sala ya se encuentra creada.");
-        } else {
-            template.opsForHash().put(actGame, "", "");
-        }
+        template.opsForSet().add(String.valueOf(roomId), "");
     }
 
     @Override
     public void removeRoom(int roomId) throws ServicesException {
-        template.delete("room:"+roomId);
+        template.delete(String.valueOf(roomId));
     }
     
 }
